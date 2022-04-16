ceb98aa4bb33333b0d520b93e5d8e04dd95f5fee
Md-Nihal <mdnihal01716@gmail.com>
2022-04-17 04:28:48 +0600

    banner and services added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
 import Header from './pages/shared/Header/Header';
 import Footer from './pages/shared/Footer/Footer';
+import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
     <Header></Header>
+    <Routes>
+    <Route path='/' element={<Home></Home>}></Route>
+    </Routes>
     <Footer></Footer>
     </div>
   );
